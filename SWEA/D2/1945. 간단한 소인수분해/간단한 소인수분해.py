0edd804a63dcde945cd8267d8b0d5efab9576a0a
JnSia <108898787+JnSia@users.noreply.github.com>
2023-07-19 00:17:57 +0900

    [D2] Title: 간단한 소인수분해, Time: 151 ms, Memory: 58,544 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -0,0 +1,29 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    N = int(input())
+
+    result = [0] * 5
+
+    while N > 1:
+        if N % 2 == 0:
+            result[0] += 1
+            N = N // 2
+        elif N % 3 == 0:
+            result[1] += 1
+            N = N // 3
+        elif N % 5 == 0:
+            result[2] += 1
+            N = N // 5
+        elif N % 7 == 0:
+            result[3] += 1
+            N = N // 7
+        elif N % 11 == 0:
+            result[4] += 1
+            N = N // 11
+    
+    print(f'#{test_case}', end=" ")
+
+    for i in result:
+        print(i, end=" ")
+    print()
