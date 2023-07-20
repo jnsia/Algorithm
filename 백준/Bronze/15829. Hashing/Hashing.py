08fa3195cecc013fd1ff4885ce26d499bd7d9032
JnSia <108898787+JnSia@users.noreply.github.com>
2023-07-21 08:58:27 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,19 @@
+# 순서가 달라져도 출력값은 일정...
+L = int(input())
+string = input()
+
+r = 31
+M = 123456891
+
+alpabet = 'abcdefghijklmnopqrstuvwxyz'
+
+hash_table = {}
+H = 0
+
+for i in range(L):
+    n = alpabet.index(string[i]) + 1
+    H += (n) * (31 ** i)
+
+hash_table[string] = H
+
+print(hash_table[string])
