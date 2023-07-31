95b6691ccf840c533aa6ae7baf79bf640b3bf8d9
JnSia <108898787+JnSia@users.noreply.github.com>
2023-07-31 21:16:16 +0900

    [Silver III] Title: N과 M (2), Time: 148 ms, Memory: 36716 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,26 @@
+def func(lst, m):
+    result = []
+
+    if m == 1:
+        for i in lst:
+            result.append([i])
+    else:
+        for i in range(len(lst)):
+            ans = [i for i in lst]
+            ans.remove(lst[i])
+
+            for j in func(ans, m - 1):
+                result.append([lst[i]] + j)
+
+    return result
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+lst = [x for x in range(1, N + 1)]
+
+for x in func(lst, M):
+    if x == sorted(x):
+        print(*x)
