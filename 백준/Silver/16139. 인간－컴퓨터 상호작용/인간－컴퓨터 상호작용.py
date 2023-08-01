8485e32972e1142ab2f62cfde73adbd7295271fb
JnSia <108898787+JnSia@users.noreply.github.com>
2023-08-01 20:11:16 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,18 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-S = input().strip()
+S = input()
 q = int(input())
-
-dp = [[0 for _ in range(len(S))] for _ in range(26)]
-
-for s in range(len(S)):
-    dp[ord(S[s]) - ord('a')][s] = 1
 
 for _ in range(q):
     a, l, r = input().split()
     l = int(l)
     r = int(r)
 
-    res = dp[ord(a) - ord('a')][l:r+1].count(1)
+    res = S[l:r+1].count(a)
     print(res)
