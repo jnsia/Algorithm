9eed4e86d39ad72b26aacd2952e0394034784c53
JnSia <108898787+JnSia@users.noreply.github.com>
2023-08-02 15:15:11 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 107 ms, Memory: 46,376 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -6,9 +6,10 @@ for t in range(1, T + 1):
     result = []
 
     for i in range(N):
-        li = list(map(int, input().split()))
-        arr.append(li)
+        lst = list(map(int, input().split()))
+        arr.append(lst)
 
+    # 가로 줄 연속된 1의 길이를 찾는다
     for x in range(N):
         count = 0
         for y in range(N):
@@ -19,13 +20,16 @@ for t in range(1, T + 1):
                 count = 0
         result.append(count)
 
-    for x in range(N):
+    # 세로 줄 연속된 1의 길이를 찾는다
+    for i in range(N):
         count = 0
-        for y in range(N):
-            if arr[y][x] == 1:
+        for j in range(N):
+            if arr[j][i] == 1:
                 count += 1
-            elif arr[y][x] == 0:
+            elif arr[j][i] == 0:
                 result.append(count)
                 count = 0
         result.append(count)
+    
+    # 길이가 M인 연속된 1의 시퀀스를 찾는다
     print(f'#{t}', result.count(M))
