4ec6efc2081f6c96c9a63e934d1f4ff6eebe9931
JnSia <108898787+JnSia@users.noreply.github.com>
2023-08-29 09:47:32 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    var answer = [0, 0];
+    
+    num_list.forEach(e => {
+        if (e % 2 != 0) {
+            answer[1] += 1  
+        } else {
+            answer[0] += 1
+        }
+    })
+    
+    return answer;
+}
