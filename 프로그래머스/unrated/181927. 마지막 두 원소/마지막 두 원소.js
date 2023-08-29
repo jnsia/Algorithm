6b149cbd2ddf3bb9fb67869716e0d567b0b25882
JnSia <108898787+JnSia@users.noreply.github.com>
2023-08-29 09:31:30 +0900

    [unrated] Title: 마지막 두 원소, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    var answer = [];
+    
+    if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+        num_list.push(num_list[num_list.length - 1] - num_list[num_list.length - 2])
+    } else {
+        num_list.push(num_list[num_list.length - 1] * 2)
+    }
+    
+    return answer = num_list;
+}
