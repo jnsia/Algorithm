93df5553e8008f320e914a870f2cb3a77e5d38cc
JnSia <108898787+JnSia@users.noreply.github.com>
2023-08-29 09:52:40 +0900

    [level 1] Title: 자릿수 더하기, Time: 2.23 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,12 @@
+function solution(n)
+{
+    var answer = 0;
+
+    while (n > 0) {
+        answer = answer + n % 10
+        n = Math.floor(n / 10)
+    }
+    console.log(n)
+
+    return answer;
+}
