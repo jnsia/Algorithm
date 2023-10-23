325d89c2708ebded4ee5f31362604907a652be50
JnSia <108898787+JnSia@users.noreply.github.com>
2023-10-23 13:04:23 +0900

    [Gold IV] Title: Σ, Time: 200 ms, Memory: 114220 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.py
+++ 백준/Gold/13172. Σ/Σ.py
@@ -1,17 +1,4 @@
-def gcd(x, y):
-    if x < y:
-        x, y = y, x
-
-    while y:
-        x, y = y, x % y
-
-    return x
-
-
 def power(num, sqr):
-    if sqr == 0:
-        return 0
-
     if sqr == 1:
         return num
 
@@ -29,17 +16,13 @@ exp_child = 0
 exp_parent = 1
 
 mod = 1000000007
+answer = 0
 
 for _ in range(M):
     parent, child = map(int, input().split())
-    exp_child = exp_parent * child + parent * exp_child
-    exp_parent = exp_parent * parent
+    answer += (child * power(parent, mod - 2) % mod) % mod
+    answer %= mod
 
-tmp_gcd = gcd(exp_parent, exp_child)
-exp_child = exp_child // tmp_gcd
-exp_parent = exp_parent // tmp_gcd
-
-answer = (exp_child * power(exp_parent, mod - 2)) % mod
 print(answer)
 
 
