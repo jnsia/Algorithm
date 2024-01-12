467e7548fc8eed358b7c6d614db16eb2dbb6dfd5
JnSia <108898787+JnSia@users.noreply.github.com>
2024-01-12 17:57:57 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T, S;
+    cin >> T >> S;
+    
+    if(S == 0 && (12 <= T && T <= 16))
+        cout << "320";
+    else
+        cout << "280";
+    
+    return 0;
+}
