0e5a415eae2c1a1a81418b4b7b3d145c1eba5b9f
JnSia <108898787+JnSia@users.noreply.github.com>
2024-01-29 20:10:41 +0900

    [level 2] Title: 주식가격, Time: 21.49 ms, Memory: 68.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    class Data {
+        int index;
+        int price;
+        
+        Data(int index, int price) {
+            this.index = index;
+            this.price = price;
+        }
+    }
+    
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        List<Data> stack = new ArrayList<>();
+        
+        stack.add(new Data(0, prices[0]));
+        
+//         System.out.println(stack.get(stack.size() - 1));
+//         System.out.println(stack.remove(stack.size() - 1));
+//         System.out.println(stack.isEmpty());
+        
+        for (int i = 1; i < prices.length; i++) {
+            Data data = new Data(i, prices[i]);
+            
+            while (!stack.isEmpty() && prices[i] < stack.get(stack.size() - 1).price) {
+                int temp = stack.remove(stack.size() - 1).index;
+                answer[temp] = i - temp;
+            }
+            
+            stack.add(data);
+        }
+        
+        while (!stack.isEmpty()) {
+            int temp = stack.remove(stack.size() - 1).index;
+            answer[temp] = prices.length - temp - 1;
+        }
+        
+        return answer;
+    }
+}
